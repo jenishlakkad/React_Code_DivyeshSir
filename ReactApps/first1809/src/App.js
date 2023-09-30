93aa351496d955f2ea5fa64ef6e64b5efaad3d2c
Jenish Lakkad <jenishlakkad7@gmail.com>
2023-09-30 12:11:42 +0530

    Modified structured

--- ReactApps/first1809/src/App.js
+++ ReactApps/first1809/src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import SecComp from './Components/SecComp';
-import FirstComp from './Components/FirstComp';
-import ThrdComp from './Components/ThrdComp';
-import ForthComp from './Components/ForthComp';
-import FunctionComp2009 from './Components/FunctionComp2009';
-import { ClassComp2009 } from './Components/ClassComp2009';
-import ClassComp2109 from './Components/ClassComp2109';
-import ClassComp2109_2 from './Components/ClassComp2109_2';
-import FunctionComp2109 from './Components/FunctionComp2109';
-import FormHendeling2409 from './Components/FormHendeling2409';
+import SecComp from './Components/FunctionComponents/SecComp';
+import FirstComp from './Components/FunctionComponents/FirstComp';
+import ThrdComp from './Components/FunctionComponents/ThrdComp';
+import ForthComp from './Components/FunctionComponents/ForthComp';
+import FunctionComp2009 from './Components/FunctionComponents/FunctionComp2009';
+import { ClassComp2009 } from './Components/ClassComponents/ClassComp2009';
+import ClassComp2109 from './Components/ClassComponents/ClassComp2109';
+import ClassComp2109_2 from './Components/ClassComponents/ClassComp2109_2';
+import FunctionComp2109 from './Components/FunctionComponents/FunctionComp2109';
+import FormHendeling2409 from './Components/FunctionComponents/FormHendeling2409';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ImageAndBase64__2709 from './Components/ImageAndBase64__2709';
-import FormHendelingClass309 from './Components/FormHendelingClass309';
-import ClassLifeCycle from './Components/ClassLifeCycle';
+import ImageAndBase64__2709 from './Components/FunctionComponents/ImageAndBase64__2709';
+import FormHendelingClass309 from './Components/ClassComponents/FormHendelingClass309';
+import ClassLifeCycle from './Components/ClassComponents/ClassLifeCycle';
 
 function App() {
   return (
