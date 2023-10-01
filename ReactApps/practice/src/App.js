abdf469b764ab7c55229501b8f51aa1bf9f9d1af
Jenish Lakkad <jenishlakkad7@gmail.com>
2023-10-01 10:21:56 +0530

    class Component

--- ReactApps/practice/src/App.js
+++ ReactApps/practice/src/App.js
@@ -4,13 +4,19 @@ import Lecture13 from './Components/Lecture13';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FormPractice1 from './Components/Practice/FormPractice1';
 import FormPractice2 from './Components/Practice/FormPractice2';
+import ClassFormPrac from './Components/Practice/ClassFormPrac';
+import HelpForm from './Components/Practice/HelpForm';
 
 function App() {
   return (
     <>
       {/* <Lecture13/> */}
       {/* <FormPractice1/> */}
-      <FormPractice2/>
+      {/* <FormPractice2/> */}
+
+      <ClassFormPrac/>
+
+      {/* <HelpForm/> */}
     </>
   );
 }
